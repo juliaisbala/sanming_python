83f5c2a8b74b7868f883da0215e8db453e156e45
juliaisbala <103523492+juliaisbala@users.noreply.github.com>
2022-04-17 15:33:52 +0800

    Add files via upload

---
+++ 100717360歐_0414_homework1.py
@@ -0,0 +1,17 @@
+import random
+number=random.randint(1,100)
+guess=-1
+start=1
+end=100
+while True:
+    guess=int(input('請輸入1~100之間的數字來猜數：'))
+    if guess == number:
+        print("猜中了，數字是：",number)
+        break
+    elif guess < number:
+        print('{}~{}之間'.format(guess,end))
+        start=guess
+    else:
+        print('{}~{}之間'.format(start,guess))
+        end=guess
+print('程式執行完畢')
